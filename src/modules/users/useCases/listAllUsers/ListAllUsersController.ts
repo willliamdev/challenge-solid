ac4cdf630eeab23afe54583dbf10a7f3cbb2cae0
William almeida <willliam.dev@gmail.com>
2022-05-28 20:00:36 -0300

    🥅 catch error

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,10 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params
-    const users = this.listAllUsersUseCase.execute({ user_id })
-    return response.json(users)
+    const { user_id } = request.headers
+    const admin_id = String(user_id)
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: admin_id })
+      return response.json(users)
+
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
+
   }
+
 }
 
 export { ListAllUsersController };
