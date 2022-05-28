0f357bfcb605df28e51650529f2e5fc7eb2de681
William almeida <willliam.dev@gmail.com>
2022-05-28 14:45:58 -0300

    controller to handle route that create a new user

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,10 +3,17 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  private createUserUseCase: CreateUserUseCase;
+  constructor(createUserUseCase: CreateUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    this.createUserUseCase.execute({ name, email });
+
+    return response.status(201).send();
   }
 }
 
