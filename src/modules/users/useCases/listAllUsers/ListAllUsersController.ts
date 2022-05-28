e5c25eecf9aa9a25c885270cbd5e894ff391996b
William almeida <willliam.dev@gmail.com>
2022-05-28 19:13:59 -0300

    Use case and controller to list users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,12 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params
+    const users = this.listAllUsersUseCase.execute({ user_id })
+    return response.json(users)
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findByEmail(user_id)
+
+    if (!user) {
+      throw new Error("user not exists")
+    }
+
+    if (user.admin) {
+      const users = this.usersRepository.list()
+      return users
+
+    } else {
+      throw new Error("This user doesn't have admin privileges")
+
+    }
   }
 }
 
