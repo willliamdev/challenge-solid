ecec48e0e135eb4c159d52e99d7183487b491c5b
William almeida <willliam.dev@gmail.com>
2022-05-28 13:58:39 -0300

    method to find a user by id

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -33,7 +33,8 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
