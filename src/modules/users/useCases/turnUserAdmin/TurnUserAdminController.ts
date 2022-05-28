3ba2bc43cfbd9d81e90964bb0fee860d17df3150
William almeida <willliam.dev@gmail.com>
2022-05-28 19:19:48 -0300

    🥅 catch error when user not exists

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -7,9 +7,16 @@ class TurnUserAdminController {
 
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params
-    const user = this.turnUserAdminUseCase.execute({ user_id })
 
-    return response.json(user)
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id })
+      return response.json(user)
+
+    } catch (error) {
+      return response.status(404).json({ error: error.message })
+
+    }
+
   }
 }
 
